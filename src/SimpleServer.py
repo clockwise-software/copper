ca16d48411e8d2dca4845aa860ba2ed18e4aeb63
SleepyAnton <grep.the.planet@gmail.com>
2021-03-31 07:50:53 +0000

    Updated SQL Command in SimpleServer.py and updated Jinja + Template

--- src/SimpleServer.py
+++ src/SimpleServer.py
@@ -1,6 +1,6 @@
-## CLOCKWISE-BOOTCAMP SimpleServer.py 
-## Based on Server from Dr. Ian Cooper @ Cardiff
-## Updated by Dr. Mike Borowczak @ UWyo March 2021
+# CLOCKWISE-BOOTCAMP SimpleServer.py
+# Based on Server from Dr. Ian Cooper @ Cardiff
+# Updated by Dr. Mike Borowczak @ UWyo March 2021
 
 import os
 from flask import Flask, redirect, request, render_template
@@ -10,16 +10,18 @@ DATABASE = 'bootcamp.db'
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def basic():
     return render_template('Employee.html')
 
-@app.route("/Employee/AddEmployee", methods = ['POST','GET'])
+
+@app.route("/Employee/AddEmployee", methods=['POST', 'GET'])
 def studentAddDetails():
-	if request.method =='GET':
+	if request.method == 'GET':
 		return render_template('EmployeeData.html')
-	if request.method =='POST':
-		firstName = request.form.get('firstName', default="Error") 
+	if request.method == 'POST':
+		firstName = request.form.get('firstName', default="Error")
 		lastName = request.form.get('lastName', default="Error")
 		businessunit = request.form.get('bu', default="Error")
 		state = request.form.get('state', default="Error")
@@ -28,7 +30,7 @@ def studentAddDetails():
 			conn = sqlite3.connect(DATABASE)
 			cur = conn.cursor()
 			cur.execute("INSERT INTO EmployeeList ('FirstName', 'LastName', 'Business Unit', 'State/Province')\
-						VALUES (?,?,?,?)",(firstName, lastName, businessunit, state ) )
+						VALUES (?,?,?,?)", (firstName, lastName, businessunit, state))
 
 			conn.commit()
 			msg = "Record successfully added"
@@ -39,29 +41,38 @@ def studentAddDetails():
 			conn.close()
 			return msg
 
-@app.route("/Employee/Search", methods = ['GET','POST'])
+
+@app.route("/Employee/Search", methods=['GET', 'POST'])
 def surnameSearch():
-	if request.method =='GET':
+	if request.method == 'GET':
 		return render_template('EmployeeSearch.html')
-	if request.method =='POST':
+	if request.method == 'POST':
 		try:
-			lastName = request.form.get('lastName', default="Error") #rem: args for get form for post
+			# rem: args for get form for post
+			city = request.form.get('City', default="Error") # Get city data from search page
+			state_prov = request.form.get('State/Province', default="Error") # Get state/province data from search page
+			skill = request.form.get('Skill', default="Error") # Get skill data from search page
+			licenses = request.form.get('Registered Licenses', default="Error") # Get license data from search page
+
 			conn = sqlite3.connect(DATABASE)
 			cur = conn.cursor()
-			cur.execute("SELECT * FROM 'EmployeeList' WHERE LastName=?", [lastName])
+			# USE 1 SQL TO SEARCH ALL FIELDS TO RETURN 1 TABLE
+			# THANK YOU SHAYA WOLF FOR YOUR HELP BUILDING THIS CRAZY STRING
+			sql_string = f"SELECT * FROM 'EmployeeList' WHERE `City` LIKE '{city if city != '' else '%'}' AND `State/Province` LIKE '{state_prov if state_prov != '' else '%'}' AND `Skill` LIKE '{skill if skill != '' else '%'}' AND `Registered Licenses` LIKE '{licenses if licenses != '' else '%'}'"
+			#print(sql_string) # Can print this string to the terminal. Useful to copy/paste as a search query here in Gitpot
+			cur.execute(sql_string)
 			data = cur.fetchall()
-			print(data)
 		except:
 			print('there was an error', data)
 			conn.close()
 		finally:
 			conn.close()
-			#return str(data)
-			return render_template('Employee.html', data = data)
+			# return str(data)
+			return render_template('Employee.html', data=data)
 
 
 
-# The name says it...
+""" # The name says it...
 @app.route("/Employee/VulnerableSearch", methods = ['GET','POST'])
 def surnameInjectionSearch():
 	if request.method =='GET':
@@ -81,7 +92,7 @@ def surnameInjectionSearch():
 		print (data)
 		print (lastName)
 		conn.close()
-		return render_template('Employee.html', data = data)
+		return render_template('Employee.html', data = data) """
 
 
 if __name__ == "__main__":
