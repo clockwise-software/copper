a1a561d5b8904d4cf5a7672ba0f358ab9f6966a0
johnschoenwolf <schoenwolf.john@gmail.com>
2021-04-03 17:16:11 +0000

    css changes

--- src/SimpleServer.py
+++ src/SimpleServer.py
@@ -10,6 +10,18 @@ DATABASE = 'bootcamp.db'
 
 app = Flask(__name__)
 
+# prevent cached responses
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
 
 @app.route("/")
 def basic():
